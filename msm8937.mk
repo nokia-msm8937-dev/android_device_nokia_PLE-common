1e9dbe39f1f42e8d54b3eda1463d4966810c2b73
atharvnegi <atharvnegi2951@gmail.com>
2024-09-09 13:05:47 +0530

    msm8937-common: Import Power HIDL from land
    Co-authored-by: Nich <nctrenco@gmail.com>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -235,7 +235,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power@1.1-service.custom \
     android.hardware.power.stats@1.0-service.mock
 
 # Public libraries
